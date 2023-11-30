c576455f9fab7d8bde4c9608c3c8a2af8213729d
Manikanta-484 <149045195+Manikanta-484@users.noreply.github.com>
2023-11-30 18:37:50 +0530

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,10 @@
+public class Account {
+    private String number();
+    private Double balance();
+    private String coustemername();
+    private String coustemeremail();
+    private String coustemerphone();
+
+    public Account() {
+    }
+}
